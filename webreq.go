55d586dcba72ab65fbb9d82088750f564b499842
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-01-10 23:53:47 -0300

    fix: unified methods for headers

--- webreq.go
+++ webreq.go
@@ -9,6 +9,20 @@ import (
 	"time"
 )
 
+type H map[string]string
+
+type Headers struct {
+	List []H
+}
+
+func NewHeaders() *Headers {
+	return &Headers{}
+}
+
+func (h *Headers) Add(key string, value string) {
+	h.List = append(h.List, H{key: value})
+}
+
 type Request struct {
 	URL         string
 	TimeOut     int
