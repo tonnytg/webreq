1cc2a8a71fd85afb12f5bab3a067bdc79456fea3
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2023-10-07 21:00:00 -0300

    fix: change from struct to map of string

--- webreq.go
+++ webreq.go
@@ -16,11 +16,13 @@ type Headers struct {
 }
 
 func NewHeaders() *Headers {
-	return &Headers{}
+	return &Headers{
+		Headers: make(map[string]string),
+	}
 }
 
 func (h *Headers) Add(key string, value string) {
-	h.List = append(h.List, H{key: value})
+	h.Headers[key] = value
 }
 
 type Request struct {
